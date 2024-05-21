70c1e7a737375662751a3dcc80d30d25f5914ea4
kara <ssctonioloriccardo@gmail.com>
2024-05-21 17:47:53 +0200

    general update

--- PyDNet-torch/Pydnet.py
+++ PyDNet-torch/Pydnet.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function
 from Blocks.DownsizingBlock import DownsizingBlock
 from Blocks.LevelConvolutionsBlock import LevelConvolutionsBlock
 import torch.nn as nn
