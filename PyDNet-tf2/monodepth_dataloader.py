77fe37e1a21cca1555317cf8f591108bce9cb4d3
kara <ssctonioloriccardo@gmail.com>
2024-05-24 13:46:33 +0200

    Update monodepth_dataloader.py

--- PyDNet-tf2/monodepth_dataloader.py
+++ PyDNet-tf2/monodepth_dataloader.py
@@ -36,21 +36,21 @@ class MonodepthDataloader(object):
         line_reader = tf.compat.v1.TextLineReader()
         _, line = line_reader.read(input_queue)
 
-        split_line = tf.string_split([line]).values
+        split_line = tf.compat.v1.string_split([line]).values
 
         # we load only one image for test, except if we trained a stereo model
         if mode == "test" and not self.params.do_stereo:
-            left_image_path = tf.string_join([self.data_path, split_line[0]])
+            left_image_path = tf.compat.v1.string_join([self.data_path, split_line[0]])
             left_image_o = self.read_image(left_image_path)
         else:
-            left_image_path = tf.string_join([self.data_path, split_line[0]])
-            right_image_path = tf.string_join([self.data_path, split_line[1]])
+            left_image_path = tf.compat.v1.string_join([self.data_path, split_line[0]])
+            right_image_path = tf.compat.v1.string_join([self.data_path, split_line[1]])
             left_image_o = self.read_image(left_image_path)
             right_image_o = self.read_image(right_image_path)
 
         if mode == "train":
             # randomly flip images
-            do_flip = tf.random_uniform([], 0, 1)
+            do_flip = tf.compat.v1.random_uniform([], 0, 1)
             left_image = tf.cond(
                 do_flip > 0.5,
                 lambda: tf.image.flip_left_right(right_image_o),
@@ -63,7 +63,7 @@ class MonodepthDataloader(object):
             )
 
             # randomly augment images
-            do_augment = tf.random_uniform([], 0, 1)
+            do_augment = tf.compat.v1.random_uniform([], 0, 1)
             left_image, right_image = tf.cond(
                 do_augment > 0.5,
                 lambda: self.augment_image_pair(left_image, right_image),
@@ -76,12 +76,14 @@ class MonodepthDataloader(object):
             # capacity = min_after_dequeue + (num_threads + a small safety margin) * batch_size
             min_after_dequeue = 2048
             capacity = min_after_dequeue + 4 * params.batch_size
-            self.left_image_batch, self.right_image_batch = tf.train.shuffle_batch(
-                [left_image, right_image],
-                params.batch_size,
-                capacity,
-                min_after_dequeue,
-                params.num_threads,
+            self.left_image_batch, self.right_image_batch = (
+                tf.compat.v1.train.shuffle_batch(
+                    [left_image, right_image],
+                    params.batch_size,
+                    capacity,
+                    min_after_dequeue,
+                    params.num_threads,
+                )
             )
 
         elif mode == "test":
@@ -98,17 +100,17 @@ class MonodepthDataloader(object):
 
     def augment_image_pair(self, left_image, right_image):
         # randomly shift gamma
-        random_gamma = tf.random_uniform([], 0.8, 1.2)
+        random_gamma = tf.compat.v1.random_uniform([], 0.8, 1.2)
         left_image_aug = left_image**random_gamma
         right_image_aug = right_image**random_gamma
 
         # randomly shift brightness
-        random_brightness = tf.random_uniform([], 0.5, 2.0)
+        random_brightness = tf.compat.v1.random_uniform([], 0.5, 2.0)
         left_image_aug = left_image_aug * random_brightness
         right_image_aug = right_image_aug * random_brightness
 
         # randomly shift color
-        random_colors = tf.random_uniform([3], 0.8, 1.2)
+        random_colors = tf.compat.v1.random_uniform([3], 0.8, 1.2)
         white = tf.ones([tf.shape(left_image)[0], tf.shape(left_image)[1]])
         color_image = tf.stack([white * random_colors[i] for i in range(3)], axis=2)
         left_image_aug *= color_image
@@ -123,13 +125,13 @@ class MonodepthDataloader(object):
     def read_image(self, image_path):
         # tf.decode_image does not return the image size, this is an ugly workaround to handle both jpeg and png
         path_length = string_length_tf(image_path)[0]
-        file_extension = tf.substr(image_path, path_length - 3, 3)
+        file_extension = tf.compat.v1.substr(image_path, path_length - 3, 3)
         file_cond = tf.equal(file_extension, "jpg")
 
         image = tf.cond(
             file_cond,
-            lambda: tf.image.decode_jpeg(tf.read_file(image_path)),
-            lambda: tf.image.decode_png(tf.read_file(image_path)),
+            lambda: tf.image.decode_jpeg(tf.compat.v1.read_file(image_path)),
+            lambda: tf.image.decode_png(tf.compat.v1.read_file(image_path)),
         )
 
         # if the dataset is cityscapes, we crop the last fifth to remove the car hood
@@ -139,7 +141,7 @@ class MonodepthDataloader(object):
             image = image[:crop_height, :, :]
 
         image = tf.image.convert_image_dtype(image, tf.float32)
-        image = tf.image.resize_images(
+        image = tf.compat.v1.image.resize_images(
             image, [self.params.height, self.params.width], tf.image.ResizeMethod.AREA
         )
 
