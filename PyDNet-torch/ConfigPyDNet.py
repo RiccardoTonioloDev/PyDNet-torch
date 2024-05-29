3d58f6860e51b101b618d5786deaae713a4c8400
kara <ssctonioloriccardo@gmail.com>
2024-05-29 11:01:27 +0200

    Created config structure for pydnet-torch

---
+++ PyDNet-torch/ConfigPyDNet.py
@@ -0,0 +1,25 @@
+class Config:
+    model_name = "PyDNet-V1-torch"
+    data_path = "/media/Volume/KITTI/"
+    filenames_file_training = "./filenames/"
+    input_height = 256
+    input_width = 512
+    batch_size = 8
+    num_epochs = 50
+    learning_rate = 1e-4
+
+    lr_loss_weight = 1
+    # Left-right consistency weight in the total loss calculation
+
+    alpha_image_loss = 0.85
+    # Weight between SSIM and L1 in the image loss
+
+    disp_gradient_loss_weight = 0.1
+    # Disparity gradient weight in the total loss calculation
+
+    output_direcotry = "./outputfiles/outputs/"
+    # Output directory to test disparities
+    checkpoint_path = "./outputfiles/checkpoints/"
+
+    retrain = True
+    # If True it retrains the model without using checkpoints
