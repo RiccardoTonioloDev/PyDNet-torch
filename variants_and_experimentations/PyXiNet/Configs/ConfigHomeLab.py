7fc8d2a218254762739ee5a03cfaf9518ca00c6b
RiccardoTonioloDev <ssctonioloriccardo@gmail.com>
2024-07-29 09:54:59 +0200

    tried celer but failed miserably

--- variants_and_experimentations/PyXiNet/Configs/ConfigHomeLab.py
+++ variants_and_experimentations/PyXiNet/Configs/ConfigHomeLab.py
@@ -41,7 +41,7 @@ class ConfigHomeLab:
         # parameter is set to True.
 
         self.checkpoint_to_use_path: str = (
-            "./outputfiles/checkpoints/betaCBAM2/betaCBAM2_e037.pth.tar"
+            "./outputfiles/checkpoints/celer1/celer1_e045.pth.tar"
         )
         # Path of the checkpoint file to be used inside the model.
 
