c2da85eb285cee518f5c3f7203f4a6a68d36172e
Riccardo <ssctonioloriccardo@gmail.com>
2024-08-17 12:17:20 +0200

    Last benchmarks results

--- variants_and_experimentations/PyXiNet/Configs/ConfigHomeLab.py
+++ variants_and_experimentations/PyXiNet/Configs/ConfigHomeLab.py
@@ -41,7 +41,7 @@ class ConfigHomeLab:
         # parameter is set to True.
 
         self.checkpoint_to_use_path: str = (
-            "./outputfiles/checkpoints/celer1/celer1_e045.pth.tar"
+            "./outputfiles/checkpoints/betaCBAM1/betaCBAM1_e048.pth.tar"
         )
         # Path of the checkpoint file to be used inside the model.
 
